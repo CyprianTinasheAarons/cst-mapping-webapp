2a88f3032cbc271033040f93fa8c7b378b190fab
Cyprian Tinashe Aarons <cyprianaarons263@gmail.com>
2024-10-25 13:49:27 +0200

    fix

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,10 +1,6 @@
-"use client";
-
 import "./globals.css";
 import { ClientRoot } from "./ClientRoot";
 import { Montserrat } from "next/font/google";
-import { createClient } from "@/utils/supabase/client";
-import { redirect } from "next/navigation";
 const montserrat = Montserrat({ subsets: ["latin"] });
 
 export default async function RootLayout({
@@ -12,14 +8,6 @@ export default async function RootLayout({
 }: {
   children: React.ReactNode;
 }) {
-  const supabase = createClient();
-  const {
-    data: { user },
-  } = await supabase.auth.getUser();
-
-  if (user) {
-    redirect("/home");
-  }
   return (
     <html lang="en">
       <body className={`flex flex-col ${montserrat.className}`}>
