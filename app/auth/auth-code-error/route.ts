935e3e47bea8f02f14a7208c5bdb28e116652851
Cyprian Tinashe Aarons <cyprianaarons263@gmail.com>
2024-10-28 11:04:15 +0200

    update

--- app/auth/auth-code-error/route.ts
+++ app/auth/auth-code-error/route.ts
@@ -1,9 +1,11 @@
 import { NextResponse } from "next/server";
-import { createClient } from "@/utils/supabase/server";
+import { createServerClient } from "@/utils/supabase/server";
+import { cookies } from "next/headers";
 
 export async function GET(request: Request) {
   const { origin } = new URL(request.url);
-  const supabase = createClient();
+  const cookieStore = cookies();
+  const supabase = createServerClient(cookieStore);
 
   await supabase.auth.signOut();
 
